dc0a61f55a8eebfebf88896be7466d721d13f76b
Magda <makneta@gmail.com>
2020-06-29 22:08:32 +0200

    Updates

---
+++ precache-manifest.7b04a511bc03c5d893f8ec834315c80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d7378da9c77771f25fb98838744d7d",
+    "url": "/color-game/index.html"
+  },
+  {
+    "revision": "d673e0321f6d75792ebb",
+    "url": "/color-game/static/css/main.f5aa4bf8.chunk.css"
+  },
+  {
+    "revision": "a60687317b5d909b68d5",
+    "url": "/color-game/static/js/2.1357a241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-game/static/js/2.1357a241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d673e0321f6d75792ebb",
+    "url": "/color-game/static/js/main.54067864.chunk.js"
+  },
+  {
+    "revision": "b9baaa418e541432d700",
+    "url": "/color-game/static/js/runtime-main.22c609a6.js"
+  }
+]);
