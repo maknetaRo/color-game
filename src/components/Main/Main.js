1683a7cdf65460afd8ac0b1b52e327b62dbe5631
maknetaRo <makneta@gmail.com>
2020-06-29 14:25:21 +0200

    First commit - start hex color game in React

---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+class Main extends Component {
+  render() {
+    return (
+      <main className="main">
+        <section class="hexCode" id="hexCode"></section>
+        <section class="first" id="first"></section>
+        <section class="second" id="second"></section>
+      </main>
+    );
+  }
+}
+
+export default Main;
